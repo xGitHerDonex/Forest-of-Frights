a963e959654832e3b13ad5b7884ac459339c771d
Quintin Scott <quintin.scott@gmail.com>
2023-09-15 14:15:58 -0400

    Update enemyAI.cs
    put in roam and update headers for house keeping/ pre-prefab tweek for individual enemies and distance/probably should place a conditional on the spawner to keep map from being overrun

--- Forest of Frights/Assets/Scripts/enemyAI.cs
+++ Forest of Frights/Assets/Scripts/enemyAI.cs
@@ -7,25 +7,36 @@ using UnityEngine.UIElements;
 
 public class enemyAI : MonoBehaviour, IDamage
 {
-    #region Fields, Members and Variables
-    [Header("-----Components-----")]
+       [Header("-----Components-----")]
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
-    [SerializeField] GameObject bullet;
     [SerializeField] Transform shootPos;
+    [SerializeField] Transform headPos;
 
     [Header("-----Enemy Stats-----")]
-    [Range(1, 10)][SerializeField] float shootRate;
-    [Range(1, 10)][SerializeField] int hp;
-    [Range(1, 10)][SerializeField] int targetFaceSpeed;
+    [SerializeField] int hp;
+    [SerializeField] int targetFaceSpeed;
+    [SerializeField] int viewAngle;
+    [SerializeField] int roamDistance;
+    [SerializeField] int roamPauseTime;
 
-    Vector3 playerDir;    
+
+    [Header("-----Gun Stats and Bullet Component -----")]
+    [SerializeField] int shootAngle;
+    [SerializeField] float shootRate;
+    [SerializeField] GameObject bullet;
+
     Vector3 pushBack;
     Vector3 playerDirection;
+    Vector3 startingPos;
+    float stoppingDistOriginal;
+    float angleToPlayer;
 
     bool playerInRange;
     bool isShooting;
-    #endregion
+    bool destinationPicked;
+
+
 
 
 
@@ -33,110 +44,112 @@ public class enemyAI : MonoBehaviour, IDamage
     // Start is called before the first frame update
     void Start()
     {
-        //Disabled updateGameGoal to not count the current enemies on screen
-        //gameManager.instance.updateGameGoal(0);
+        startingPos = transform.position;
+        stoppingDistOriginal = agent.stoppingDistance;
+        gameManager.instance.updateGameGoal(1);
 
     }
-    #region Update
-    /* 
-     * if the player is in range of the enemy get the player position from the game manager instance running and subtract my enemies position from it for a direction
-     * if the nav mesh distance between its current position and the player destination is than or equal to the stopping distance from the enemy
-     * face the target
-     * and if he isnt shooting then start the sub routine to shoot at the object
-     * then the Nav mesh calcutates a new path to the destination if it has moved must return true 
-     *  set destination otherwise returns false and no new path is calculated
-     */
 
     // Update is called once per frame
     void Update()
     {
-        if (playerInRange)
+        if (playerInRange && !canSeePlayer())
         {
+            StartCoroutine(roam());
+        } else if (!playerInRange)
+        {
+            StartCoroutine(roam());
+        }
 
-            playerDirection = gameManager.instance.player.transform.position - transform.position;
+    }
+    IEnumerator roam()
+    {
+        if (agent.remainingDistance < 0.05f && !destinationPicked)
+        {
+            destinationPicked = true;
+            yield return new WaitForSeconds(roamPauseTime);
 
+            Vector3 randomPos = Random.insideUnitSphere * roamDistance;
+            randomPos += startingPos;
+            NavMeshHit destination;
+            NavMesh.SamplePosition(randomPos, out destination, roamDistance, 1);
+            agent.SetDestination(destination.position);
 
-            if (agent.remainingDistance <= agent.stoppingDistance)
-            {
-                faceTarget();
-                if (!isShooting)
-                {
-                    StartCoroutine(shoot());
-                }
-            }
-
-            agent.SetDestination(gameManager.instance.player.transform.position);
+            destinationPicked = false;
         }
     }
-
-    #endregion
-
-    #region if the enemy takes damage
-
-    /*
-     * if the enemy takes damage requires an amount for the damage in a whole number
-     * then subtracts the amount of damage from that whole number
-     * the call the sub routine to run at the same time tomake the enemy feedback show (flashing damage indicator)
-     * also if the health is less than or equal to 0 destroy this enemy
-     * 
-     */
     public void takeDamage(int amount)
     {
         hp -= amount;
+       
         StartCoroutine(flashDamage());
         if (hp <= 0)
         {
-            gameManager.instance.updateGameGoal(+1); //updates win condition set at 10 or greater "You win" also increments enemies killed and starts the win table Ienum
+            gameManager.instance.updateGameGoal(-1);
             Destroy(gameObject);
         }
 
-    } 
-    #endregion
-
-    #region Enemy Feedback upon taking damage
-    //changes the material color from the original material to a red color for .1 seconds
-    //the changes the color back to its original white state.
+    }
+    
     IEnumerator flashDamage()
     {
-              
-    
         model.material.color = Color.red;
         yield return new WaitForSeconds(0.1f);
         model.material.color = Color.white;
-       
-    } 
-    #endregion
+    }
+    bool canSeePlayer()
+    {
 
-    #region Shoot
-    // Set the shooting bool to true then
-    // places and intializes the bullet object from the shooting postion and gives it a direction while triggering the bool that checks whether the enemy is shooting or not.
-    //suspends the coroutine for the amount of seconds the shootrate is set to
+        playerDirection = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDirection.x, 0, playerDirection.z) , transform.forward);
+#if(UNITY_EDITOR)
+        Debug.Log(angleToPlayer);
+        Debug.DrawRay(headPos.position, playerDirection);
+#endif
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDirection, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewAngle)
+            {
+                agent.stoppingDistance = stoppingDistOriginal;
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    faceTarget();
+                    if (!isShooting && angleToPlayer <= shootAngle)
+                    {
+                        StartCoroutine(shoot());
+                    }
+                }
+                return true;
+            }
+        }
+        agent.stoppingDistance = 0;
+        return false;
+    }
+
     IEnumerator shoot()
     {
         isShooting = true;
         Instantiate(bullet, shootPos.position, transform.rotation);
         yield return new WaitForSeconds(shootRate);
         isShooting = false;
-    } 
-    #endregion
+    }
 
-    #region Face the Target
-
-    //sets the rotation of the enemy to face the player based on the player direction to the enemy 
-    //and it lerps the rotation over time so it is smooth and not choppy
     void faceTarget()
     {
         Quaternion rotation = Quaternion.LookRotation(playerDirection);
         //lerp over time rotation
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * targetFaceSpeed);
-    } 
-    #endregion
+    }
 
-    #region On Trigger Enter
+    public void physics(Vector3 dir)
+    {
+        agent.velocity += dir;
+    }
 
-    //if an object enters the collider for the enemy check to see if it is the Player
-    //if it is the player set player in range bool to true
-    //returns nothing
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -145,23 +158,15 @@ public class enemyAI : MonoBehaviour, IDamage
 
         }
     }
-    #endregion
 
-    #region On Trigger Exit
-    // does the exact opposite as On Trigger enter
-    // it checks to see if the object that is in the collider is the player if it isnt the 
     public void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
+            agent.stoppingDistance = 0;
 
         }
-    }
-    #endregion
-    public void physics(Vector3 dir)
-    {
-        agent.velocity += dir;
     }
 
 
