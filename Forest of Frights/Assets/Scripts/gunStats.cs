09c02f9010975daca82d2713f0cc2cbdad299862
Rafael Vargas <rafaelvargas.85@gmail.com>
2023-09-29 04:48:24 -0400

    Gun Recoil Added
    Implemented a Gun Recoil and added parameter for gunStats to make it applicable to all weapons

--- Forest of Frights/Assets/Scripts/gunStats.cs
+++ Forest of Frights/Assets/Scripts/gunStats.cs
@@ -11,11 +11,13 @@ public class gunStats : ScriptableObject
     [Header("----- Gun Stats -----")]
     public float shootRate;
     public int shootDamage;
+    public float recoilAmount;
     public int shootDist;
     public int ammoCur;
     public int ammoMax;
 
     public GameObject model;
     public ParticleSystem hitEffect;
+    public ParticleSystem shootEffect;
     public AudioClip shotSound;
 }
