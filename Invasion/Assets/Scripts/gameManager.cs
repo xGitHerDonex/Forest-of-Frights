044cdd16d30e54c1075180e2dfc09b8b9f922b8b
andrewsipes <andrew.sipes@outlook.com>
2023-10-07 10:50:41 -0400

    Revert "Added Level 3 terrain and updated player prefab for speed and size."
    This reverts commit c2a436d9e559135402be94c2500fd7dd5033093a.

--- Invasion/Assets/Scripts/gameManager.cs
+++ Invasion/Assets/Scripts/gameManager.cs
@@ -20,6 +20,7 @@ public class gameManager : MonoBehaviour
     [SerializeField] GameObject playerStam;
     [SerializeField] Image playerLeftStamBar;
     [SerializeField] Image playerRightStamBar;
+    [SerializeField] GameObject enemiesRemainingUI;
     [SerializeField] GameObject reticle;
 
 
