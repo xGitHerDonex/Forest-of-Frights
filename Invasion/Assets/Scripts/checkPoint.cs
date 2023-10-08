e6024de2237d9adbc84f830eb07fc542a54d9df1
andrewsipes <andrew.sipes@outlook.com>
2023-10-08 18:29:00 -0400

    Bug fixes on Last boss, Goro

--- Invasion/Assets/Scripts/checkPoint.cs
+++ Invasion/Assets/Scripts/checkPoint.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class checkPoint : MonoBehaviour
 {
-    
+    bool isTriggered;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && gameManager.instance.player.transform.position != transform.position)
+        if (!isTriggered && other.CompareTag("Player") && gameManager.instance.player.transform.position != transform.position)
         {
             gameManager.instance.playerSpawnPos.transform.position = transform.position;
             StartCoroutine(gameManager.instance.checkPointPopup());
-
+            isTriggered = true;
         }
 
 
