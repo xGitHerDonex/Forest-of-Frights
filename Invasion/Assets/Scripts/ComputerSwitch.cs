9938a0bdce009db906f23b663be56adcfd086826
Rafael Vargas <rafaelvargas.85@gmail.com>
2023-10-10 05:09:35 -0400

    Level 2 Rework
    Added Computer switches that after activated with E, open the doors to the next sections and makes an activation sound.  Specimen tanks updated.

---
+++ Invasion/Assets/Scripts/ComputerSwitch.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ComputerSwitch : MonoBehaviour
+{
+    public TextMeshProUGUI activateMessage;
+    public Material greenMaterial;
+    public GameObject door;
+    public GameObject monitor;
+    public AudioClip activateSound;
+
+    private bool isInRange = false;
+    private bool isSwitchActivated = false;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        activateMessage.gameObject.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (isInRange && !isSwitchActivated)
+        {
+            activateMessage.gameObject.SetActive(true);
+
+            if (Input.GetButtonDown("Activate"))
+            {
+                Renderer monitorRender = monitor.GetComponent<Renderer>();
+                if (monitorRender != null)
+                {
+                    monitorRender.material = greenMaterial;
+                    Destroy(door);
+                    isSwitchActivated = true;
+                    activateMessage.gameObject.SetActive(false);
+                    audioSource.PlayOneShot(activateSound);
+                }
+            }
+
+            
+
+
+        }
+        //else
+        //{
+        //    activateMessage.gameObject.SetActive(false);
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = false;
+            activateMessage.gameObject.SetActive(false);
+        }
+    }
+}
